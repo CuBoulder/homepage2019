6bf13120344285bc626d9bb4cf72404d3ea656bf
Kevin Crafts <kevin.crafts@colorado.edu>
2019-01-17 12:00:07 -0700

    fixing wrong theme name

--- template.php
+++ template.php
@@ -19,10 +19,10 @@ function homepage2019_preprocess_html(&$vars) {
 
 
   // SET BANNER COLOR (banner-white, banner-light, banner-cumodern, banner-black)
-  $banner_color = theme_get_setting('banner_color', 'homepage2019') ? theme_get_setting('banner_color', 'homepage2017_theme') : 'black';
+  $banner_color = theme_get_setting('banner_color', 'homepage2019') ? theme_get_setting('banner_color', 'homepage2019') : 'black';
   //$banner_color = 'light';
   $vars['classes_array'][]='banner-' . $banner_color;
-  $layout = theme_get_setting('layout_style', 'homepage2019') ? theme_get_setting('layout_style', 'homepage2017_theme') : 'layout-wide';
+  $layout = theme_get_setting('layout_style', 'homepage2019') ? theme_get_setting('layout_style', 'homepage2019') : 'layout-wide';
   $vars['classes_array'][]=$layout;
   unset($vars['head_title_array']['slogan']);
   $vars['head_title'] = implode(' | ', $vars['head_title_array']);
